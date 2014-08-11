1b635c7bf7047582db5a009bccb605f6945871e1
QuickApps <info@quickapps.es>
2014-08-11 17:10:37 +0200

    update

--- webroot/index.php
+++ webroot/index.php
@@ -1,51 +1,45 @@
 <?php
 /**
- * Index
- *
- * The Front Controller for handling every request
- *
- * PHP 5
- *
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
+ * Licensed under The GPL-3.0 License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         CakePHP(tm) v 0.2.9
- * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @since	 2.0.0
+ * @author	 Christopher Castro <chris@quickapps.es>
+ * @link	 http://www.quickappscms.org
+ * @license	 http://opensource.org/licenses/gpl-3.0.html GPL-3.0 License
  */
-// for built-in server
+
+/**
+ * For built-in server
+ */
 if (php_sapi_name() === 'cli-server') {
 	$_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
 
-	$url = urldecode($_SERVER['REQUEST_URI']);
-	$file = __DIR__ . $url;
-	if (strpos($url, '..') === false && strpos($url, '.') !== false && is_file($file)) {
+	$url = parse_url(urldecode($_SERVER['REQUEST_URI']));
+	$file = __DIR__ . $url['path'];
+	if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
 		return false;
 	}
 }
 
 /**
- * Full path to your website directory, WITHOUT a trailing DS.
+ * Full path to your website directory, WITHOUT a trailing DIRECTORY_SEPARATOR. In other words, the directory 
+ * which holds your site's "webroot" directory.
+ *
+ * You should never change this.
  */
 define('SITE_ROOT', dirname(__DIR__));
 
 /**
- * Path to composer's vendor directory.
- * There is where quickapps & cakephp must be located.
+ * Path to the "src" directory where QuickApps CMS core resides, WITH a trailing DIRECTORY_SEPARATOR.
+ *
+ * You can change this when your QuickApps CMS core is in a different location.
+ * Useful on a shared core installation. 
  */
-define('VENDOR_INCLUDE_PATH', SITE_ROOT . '/vendor');
+define('QUICKAPPS_PATH', SITE_ROOT . '/vendor/quickapps/cms/src/');
 
-/**
- * Path to the QuickApps directory.
- */
-define('QUICKAPPS_CORE_INCLUDE_PATH', VENDOR_INCLUDE_PATH . '/quickapps/cms/src');
-
-require QUICKAPPS_CORE_INCLUDE_PATH . '/Config/bootstrap.php';
+require QUICKAPPS_PATH . 'Config/bootstrap.php';
 
 use Cake\Network\Request;
 use Cake\Network\Response;
@@ -56,4 +50,3 @@ $dispatcher->dispatch(
 	Request::createFromGlobals(),
 	new Response()
 );
-
