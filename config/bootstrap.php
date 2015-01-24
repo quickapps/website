fc56d1335bcbfcce3a6a23963d2e991758869c1c
QuickApps <info@quickapps.es>
2015-01-24 03:47:59 +0100

    added license

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,8 +1,19 @@
 <?php
 /**
+ * Licensed under The GPL-3.0 License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since    2.0.0
+ * @author   Christopher Castro <chris@quickapps.es>
+ * @link     http://www.quickappscms.org
+ * @license  http://opensource.org/licenses/gpl-3.0.html GPL-3.0 License
+ */
+
+/**
  * You can use this file to set specific logic outside the QuickApps CMS core,
- * so updates won't affect them. Warning: This is for advanced users only, those with a
- * proper understanding of PHP and its syntax.
+ * so updates won't affect them. Warning: This is for advanced users only, those
+ * with a proper understanding of PHP and its syntax.
  *
  * NOTE: This file is included after QuickApps CMS bootstrap.php file.
  */
