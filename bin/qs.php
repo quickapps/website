55b31acfdf16bf498b1ce29ca71d573dd858c9a5
QuickApps <info@quickapps.es>
2015-01-24 03:48:16 +0100

    added shell console

---
+++ bin/qs.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php -q
+<?php
+/**
+ * Licensed under The GPL-3.0 License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @since    2.0.0
+ * @author   Christopher Castro <chris@quickapps.es>
+ * @link     http://www.quickappscms.org
+ * @license  http://opensource.org/licenses/gpl-3.0.html GPL-3.0 License
+ */
+require dirname(__DIR__) . '/config/paths.php';
+include QUICKAPPS_CORE . 'config/bootstrap.php';
+
+exit(Cake\Console\ShellDispatcher::run($argv));
